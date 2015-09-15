c963ace26669104751faa232405adb5f0b2a156e
gtxll8 <george@chessborg.com>
2015-09-15 23:02:30 +0100

    Changes after review final

--- models.py
+++ models.py
@@ -78,7 +78,7 @@ class ConferenceForm(messages.Message):
 class Session(ndb.Model):
     """Session -- Session object"""
     name            = ndb.StringProperty(required=True)
-    speaker         = ndb.StringProperty()
+    speaker         = ndb.KeyProperty(kind='Speaker')
     highlights      = ndb.StringProperty(repeated=True)
     duration        = ndb.IntegerProperty()
     typeOfSession   = ndb.StringProperty(choices=['workshop', 'keynotes', 'breakout'])  # list with possible types of sessions
