ba9ba19ebd7365de397d2ff9438785c6d734130e
gtxll8 <george@chessborg.com>
2015-09-16 14:51:57 +0100

    after code review

--- conference.py
+++ conference.py
@@ -115,7 +115,7 @@ WISHLIST_POST_REQUEST = endpoints.ResourceContainer(
 SESSION_START_TIME_GET_REQUEST = endpoints.ResourceContainer(
     message_types.VoidMessage,
     websafeConferenceKey=messages.StringField(1),
-    startTime=messages.DateTimeField(2, input_formats=['%H:%M']),
+    startTime=messages.DateTimeField(2),
 )
 
 SESSION_HIGHLIGHTS_GET_REQUEST = endpoints.ResourceContainer(
